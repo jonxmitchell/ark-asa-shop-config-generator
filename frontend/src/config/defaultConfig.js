87630d82bee30609735f6ece0f7a135e3bfcff8b
Jon Mitchell <jonxmitchell@outlook.com>
2024-07-26 16:40:01 +0100

    New file to contain the default config

---
+++ frontend/src/config/defaultConfig.js
@@ -0,0 +1,39 @@
+// src/config/defaultConfig.js
+
+export const defaultConfig = {
+	Mysql: {
+		UseMysql: false,
+		MysqlHost: "",
+		MysqlUser: "",
+		MysqlPass: "",
+		MysqlDB: "",
+		MysqlPort: 3306,
+	},
+	General: {
+		Discord: {
+			Enabled: false,
+			SenderName: "ArkShop",
+			URL: "",
+		},
+		TimedPointsReward: {
+			Enabled: false,
+			StackRewards: false,
+			Interval: 30,
+			Groups: {},
+		},
+		ItemsPerPage: 15,
+		ShopDisplayTime: 15.0,
+		ShopTextSize: 1.3,
+		DbPathOverride: "",
+		DefaultKit: "",
+		GiveDinosInCryopods: true,
+		UseSoulTraps: false,
+		CryoLimitedTime: false,
+		CryoItemPath: "",
+		UseOriginalTradeCommandWithUI: false,
+		PreventUseNoglin: true,
+		PreventUseUnconscious: true,
+		PreventUseHandcuffed: true,
+		PreventUseCarried: true,
+	},
+};
